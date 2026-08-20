8099f4f0070d55662cf43093d533391d6dc38736
Emilio Justin <emilio.justin54@gmail.com>
2026-08-20 16:09:16 +0700

    feat(bonus): add TOTP MFA security foundation

---
+++ auth-provider/server/src/mfa/mfa.constants.ts
@@ -0,0 +1,6 @@
+export const MFA_TOTP_ISSUER = 'Labpro Auth Provider';
+export const MFA_TOTP_DIGITS = 6;
+export const MFA_TOTP_PERIOD_SECONDS = 30;
+export const MFA_TOTP_WINDOW = 1;
+export const MFA_TOTP_SECRET_BYTES = 20;
+export const MFA_SECRET_IV_BYTES = 12;
